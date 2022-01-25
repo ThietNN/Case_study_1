dc10b6e9d6fc46a82c9e39db034b339a377ee113
ThietNN <nguyenthiet1225@gmail.com>
2022-01-25 17:50:26 +0700

    added new js file

---
+++ Case_Study_1.js
@@ -0,0 +1,126 @@
+// khai báo canvas
+let game = document.getElementById("game");
+let ctx = game.getContext("2d");
+let scoreboard = document.getElementById("score");
+let record = document.getElementById("highest");
+
+// khai báo biến ảnh
+let char = new Image();
+let background = new Image();
+let upperPipe = new Image();
+let lowerPipe = new Image();
+
+// //khai báo nguồn ảnh
+
+// function set() {
+//     switch (chooseChar) {
+//         case 1:
+//             char.src = "images/Doraemon.png";
+//             break;
+//         case 2:
+//             char.src = "images/Classic.png";
+//             break;
+//     }
+//     run();
+// }
+
+
+char.src = "images/Doraemon.png";
+background.src="images/background_2.png";
+upperPipe.src="images/Upper_Pipe.png";
+lowerPipe.src="images/Lower_Pipe.png";
+
+
+//khai báo khoảng cách 2 ống trên/dưới, score ban đầu
+let gap = 150;
+let distanceToLower = 200 + gap;
+let score =0;
+let max = 0;
+
+//đặt vị trí nhân vật
+let charPosition={
+    x : background.width /5,
+    y : background.height /2,
+    width : 50,
+    height : 50
+}
+
+//function bay
+document.addEventListener("keydown",fly)
+function fly(){
+    charPosition.y -=45;
+}
+
+//khai báo vị trí pipe
+let pipe = [];
+pipe[0] = {
+    x:game.width,
+    y: 0
+}
+
+//khoảng cách giữa các pipe, tốc độ xuất hiện pipe
+let speed = 400
+
+//vẽ ảnh
+function run(){
+    //vẽ background và nhân vật
+    ctx.drawImage(background,0,0);
+    ctx.drawImage(char,charPosition.x,charPosition.y,charPosition.width,charPosition.height);
+
+    // tăng tốc khi đạt mốc điểm (DỰ KIẾN)
+
+    //vẽ pipe
+    for(let i = 0; i<pipe.length;i++){
+        ctx.drawImage(upperPipe,pipe[i].x,pipe[i].y,50,200);
+        ctx.drawImage(lowerPipe,pipe[i].x,pipe[i].y+distanceToLower,50,200);
+        pipe[i].x --;
+
+        //thêm pipe mới
+        if(pipe[i].x == speed){
+            pipe.push({
+                x:game.width,
+                y:Math.floor(Math.random()*200)-200
+            })
+        }
+
+        //điều kiện thua
+        if(charPosition.y+charPosition.height==game.height||
+
+            charPosition.x+charPosition.width>= pipe[i].x && charPosition.x <= pipe[i].x +50
+            && (charPosition.y<=pipe[i].y+200||
+
+                charPosition.y +charPosition.height>= pipe[i].y+ distanceToLower)
+        ){
+            // reset score, không alert để giữ highest score
+            score = 0;
+
+            //reset score và highest score
+            // alert ("Game over")
+            // return;
+        }
+
+        // score ++ khi qua pipe thành công
+        if(pipe[i].x == charPosition.x){
+            score ++;
+        }
+
+        //lấy highest score (record)
+        if (score > max){
+            max = score
+        }
+
+
+    }
+
+
+    //hiện điểm
+    scoreboard.innerText = "score: " + score;
+    record.innerText = "Highest Score: " + max;
+
+    // trọng lực
+    charPosition.y += 0.5;
+
+    requestAnimationFrame(run);
+}
+run();
+
